d3a3a282b64b462dcdaeea85fb7bf8477388f785
Anton Batiaev <anton@batiaev.com>
2024-01-22 14:29:15 +0000

    show example of components and local storage

--- src/App.js
+++ src/App.js
@@ -4,63 +4,67 @@ import TankList from "./components/TankList";
 import TankForm from './components/TankForm';
 import MyModal from "./components/UI/MyModal";
 import TankFilter from "./components/TankFilter";
-import cl from "./styles/App.css";
-
 
 function App() {
-  
-  const [tanks, setTanks] = useState([
-    {id:1, title:"Счет в Офшорах",type:"текущие счета",bank:"ВТБ" ,body:"788897"},
-    {id:2, title:"Вклад в Сбербанке",type:"бюджетные счета",bank:"СберБанк",body:"23500"},
-    {id:3, title:"Кредитка",type:"счета службы судебных приставов",bank:"Открытие" ,body:"-5000"},
-  ])
+    useMemo(() => {
+        let data = localStorage.getItem('tanks');
+        if (data === undefined || data === null || data === []) {
+            const data = [
+                {id: 1, title: "Счет в Офшорах", type: "текущие счета", bank: "ВТБ", body: "788897"},
+                {id: 2, title: "Вклад в Сбербанке", type: "бюджетные счета", bank: "СберБанк", body: "23500"},
+                {id: 3, title: "Кредитка", type: "счета службы судебных приставов", bank: "Открытие", body: "-5000"},
+            ]
+            localStorage.setItem('tanks', JSON.stringify(data));
+        }
+    }, []);
 
-  
-const[filter, setFilter] = useState({sort: '', query: ''})
-const [modal, setModal] = useState(false);
+    const [tanks, setTanks] = useState(JSON.parse(localStorage.getItem('tanks')));
 
-/*bfgbf*/
-const sortedTanks = useMemo(() =>{
-  console.log('Отработано')
-if (filter.sort) {
-return [...tanks].sort((a,b)=>a[filter.sort].localeCompare(b[filter.sort]));
-}
-return tanks;
-}, [filter.sort, tanks])
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false);
 
-const sortedAndSearchedTanks = useMemo(()=>{
-  return sortedTanks.filter(tank => tank.title.toLowerCase().includes(filter.query.toLowerCase()))
-}, [filter.query, sortedTanks])
+    const sortedTanks = useMemo(() => {
+        console.log('Отработано')
+        if (filter.sort) {
+            return [...tanks].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+        }
+        localStorage.setItem('tanks', JSON.stringify(tanks));
+        return tanks;
+    }, [filter.sort, tanks])
 
-const createTank = (newTank) => {
-setTanks([...tanks, newTank])
-setModal(false)
-}
+    const sortedAndSearchedTanks = useMemo(() => {
+        return sortedTanks.filter(tank => tank.title.toLowerCase().includes(filter.query.toLowerCase()))
+    }, [filter.query, sortedTanks])
 
-const removeTank = (tank) => {
-  setTanks(tanks.filter(p => p.id !==tank.id))
-}
+    const createTank = (newTank) => {
+        setTanks([...tanks, newTank])
+        setModal(false)
+    }
 
-  
-  return(
-    
-    <div className='App'> 
-    <MyButton  style ={{marginTop: 30 }}onClick ={()=> setModal(true)}>
-Создать счет
-    </MyButton>
-    <MyModal visible={modal} setVisible={setModal}>
-      <TankForm create ={createTank}/>
-      </MyModal>
+    const removeTank = (tank) => {
+        setTanks(tanks.filter(p => p.id !== tank.id))
+    }
 
-<hr style={{margin: '15px 0'}}/>
-<TankFilter 
-filter={filter} 
-setFilter={setFilter}
-/>
-  <TankList remove={removeTank} tanks={sortedAndSearchedTanks} title="Список счетов"/>
-    </div>
-    );  
-        };
-  
- 
+
+    return (
+
+        <div className='App'>
+            <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+                Создать счет
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <TankForm create={createTank}/>
+            </MyModal>
+
+            <hr style={{margin: '15px 0'}}/>
+            <TankFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+            <TankList remove={removeTank} tanks={sortedAndSearchedTanks} title="Список счетов"/>
+        </div>
+    );
+};
+
+
 export default App;
--- src/components/TankItem.jsx
+++ src/components/TankItem.jsx
@@ -1,33 +1,54 @@
-
 import React from "react";
 import MyButton from "./UI/MyButton";
+import {Button} from "@mui/material";
 import DetailsButton from "./DetailsButton";
+import {SimpleDialog} from "./MyTitle/SimpleDialog";
 
 const TankItem = (props) => {
-    
-return(
-    <div className="tank">
-  <div className="tank__content">
-    <strong>{props.number}. {props.tank.title}</strong>
-    <div>
-      Тип: {props.tank.type}
-    </div>
-    <div>
-      Банк: {props.tank.bank}
-    </div>
-    <div>
-      Баланс: {props.tank.body}
-    </div>
-    
-  </div>
-  <div className='tank__btns'>
-    <DetailsButton tank={props.tank}/>
-      
-    <MyButton onClick ={()=> props.remove(props.tank)}>Удалить</MyButton>
-  </div>
-</div>
-   
-    
+    const [open, setOpen] = React.useState(false);
+    const [selectedValue, setSelectedValue] = React.useState(props.tank);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = (value) => {
+        setOpen(false);
+        setSelectedValue(value);
+    };
+
+    return (
+        <div className="tank">
+            <div className="tank__content">
+                <strong>{props.number}. {props.tank.title}</strong>
+                <div>
+                    Тип: {props.tank.type}
+                </div>
+                <div>
+                    Банк: {props.tank.bank}
+                </div>
+                <div>
+                    Баланс: {props.tank.body}
+                </div>
+            </div>
+
+            <div className='tank__btns'>
+                <Button variant="text"
+                        onClick={handleClickOpen}>
+                    Детали
+                </Button>
+                <SimpleDialog
+                    tank={selectedValue}
+                    open={open}
+                    onClose={handleClose}
+                />
+                <DetailsButton tank={props.tank}/>
+
+                <MyButton onClick={() => props.remove(props.tank)}>Удалить</MyButton>
+            </div>
+        </div>
+
+
     );
 };
 
